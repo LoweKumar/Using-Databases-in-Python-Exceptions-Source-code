d4e9657e7aa4276b6fa7f19d5a5b266fac0ceae1
LoweKumar <lowekr00@gmail.com>
2022-09-27 21:02:52 +0530

    Lecture 383- Exceptions - ValueError, EOFError, ZeroDivisionError

--- examples.py
+++ examples.py
@@ -4,19 +4,12 @@ def factorial(n):
     if n <= 1:
         return 1
     else:
-        print(n / 0)
         return n * factorial(n-1)
 
-# try:
-#     print(factorial(900))
-# except (RecursionError, OverflowError):
-#     print("This program cannot calculate factorials that large")
-#
-# print("Program terminating")
 try:
-    print(factorial(1000))
-except (RecursionError, ZeroDivisionError):
-    print("This program calculate factorials that large")
+    print(factorial(900))
+except (RecursionError, OverflowError):
+    print("This program cannot calculate factorials that large")
 
-print("Program Terminating")
+print("Program terminating")
 
